ab26c900c7922c8d3800c92fa80960b134a54f4b
brian moore <datastx@gmail.com>
2023-02-20 15:24:53 -0500

    add timeout for test

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ run:
 	go run src/main.go
 
 test:
-	go test ./...
+	go test ./... -timeout 10s
--- src/tower/tower_test.go
+++ src/tower/tower_test.go
@@ -1,1 +1,88 @@
 package tower
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/datastx/FileTower/src/schema"
+	"github.com/fsnotify/fsnotify"
+)
+
+func TestRun(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "tower-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer watcher.Close()
+
+	ch := make(chan schema.Record)
+
+	done := make(chan bool)
+	go func() {
+		Run([]string{tempDir}, watcher, ch)
+		done <- true
+	}()
+
+	// create a test file
+	testFile := filepath.Join(tempDir, "test.txt")
+	if err := ioutil.WriteFile(testFile, []byte("test"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for events to be processed
+	expected := []schema.Record{
+		{Operation: fsnotify.Create, FileName: testFile},
+	}
+	for i := 0; i < len(expected); i++ {
+		select {
+		case actual := <-ch:
+			if actual != expected[i] {
+				t.Errorf("Unexpected record received: got %v, want %v", actual, expected[i])
+			}
+		}
+	}
+
+	// write to the test file
+	if err := ioutil.WriteFile(testFile, []byte("test2"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for events to be processed
+	expected = append(expected, schema.Record{Operation: fsnotify.Write, FileName: testFile})
+	for i := 1; i < len(expected); i++ {
+		select {
+		case actual := <-ch:
+			if actual != expected[i] {
+				t.Errorf("Unexpected record received: got %v, want %v", actual, expected[i])
+			}
+		}
+	}
+
+	// remove the test file
+	if err := os.Remove(testFile); err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for events to be processed
+	expected = append(expected, schema.Record{Operation: fsnotify.Remove, FileName: testFile})
+	for i := 2; i < len(expected); i++ {
+		select {
+		case actual := <-ch:
+			if actual != expected[i] {
+				t.Errorf("Unexpected record received: got %v, want %v", actual, expected[i])
+			}
+		}
+	}
+
+	// stop the watcher
+	watcher.Close()
+	<-done
+}
